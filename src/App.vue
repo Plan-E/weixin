7ece7196dced7af4e7287ed378f90bc3c0ad1341
YX <984637230@qq.com>
2023-03-14 16:44:03 +0800

    uniapp小程序初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+	view{
+		font-size: 28rpx;
+		color: #000;
+		box-sizing: border-box;
+	}
+	image{
+		display: block;
+		height: auto;
+	}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<u-search placeholder="日照香炉生紫烟" v-model="keyword"></u-search>		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				keyword: ''
+			}
+		},
+		onLoad() {
+			console.log(uni.$u.config.v);
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style >
+	
+	
+</style>
